62002b964d7d220833fdbfd78ae9b438784ac8d5
Brilian <bfatruan16@gmail.com>
2026-09-11 12:51:57 +0900

    Revert "feat: fitur "Koneksi WiFi Toko" -- sambung WiFi dari dalam app tanpa buka Pengaturan Windows"
    This reverts commit 72f181e157fea1b91b776af643cdcea8299a7029.

--- preload.js
+++ preload.js
@@ -13,15 +13,4 @@ contextBridge.exposeInMainWorld('gloriaDesktop', {
   isElectron: true,
   printSilently: (html, deviceName) => ipcRenderer.invoke('print-silent', html, deviceName),
   listPrinters: () => ipcRenderer.invoke('list-printers'),
-  // 11 Sep 2026: jembatan "Koneksi WiFi Toko" -- lihat setupWifiHandlers() di
-  // main.js. Halaman (GloriaBilliard.html, kode SAMA yg jg dipakai di browser
-  // biasa) WAJIB feature-detect method2 ini satu-satu (bukan cuma isElectron)
-  // sebelum dipakai -- konten web di-update terpisah & lebih sering dari shell
-  // Electron ini (lihat checkForContentUpdate di main.js), jadi ada jendela
-  // waktu konten BARU dimuat di shell LAMA yg belum punya method2 ini.
-  listWifiProfiles: () => ipcRenderer.invoke('wifi-list-profiles'),
-  getWifiStatus: () => ipcRenderer.invoke('wifi-status'),
-  connectWifi: (profileName) => ipcRenderer.invoke('wifi-connect', profileName),
-  getSavedWifiProfile: () => ipcRenderer.invoke('wifi-get-saved-profile'),
-  setSavedWifiProfile: (profileName) => ipcRenderer.invoke('wifi-set-saved-profile', profileName),
 });
